8077177f44ae4191f597d37d1c899ccab7d406c2
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2023-11-03 03:48:29 -0700

    Exercise 3.1 Prompt: Implement a Node application that returns a hardcoded list of phonebook entries The address should be http://localhost:3001/api/persons. The application must be started with the command npm start. The application must also offer an npm run dev command. That should run the application and restart the server on changes.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("Ready to create the app");
+// Express request
+const express = require('express');
+const app = express();
+
+// Initial data
+const persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+// The notes JSON API response
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+// Serving the backend to the browser
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
