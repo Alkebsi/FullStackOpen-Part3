deff8a37d32bbc56ff5378dd5262a60d640727ed
Mohammed Alkebsi <moha.alkebsi@gmail.com>
2023-11-07 00:47:11 -0800

    Updated the PORT so that it will take into account the environment variable PORT if any was givin!

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.post('/api/persons', (request, response) => {
 });
 
 // Serving the backend to the browser
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
